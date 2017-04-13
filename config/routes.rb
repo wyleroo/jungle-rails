52990052c95acb78197ceec4cd7347458cd4e1aa
Wyatt Robinson <thewyatt@gmail.com>
2017-04-13 20:55:46 +0000

    Added sessions controller. Updated users controller. Added views for login.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
