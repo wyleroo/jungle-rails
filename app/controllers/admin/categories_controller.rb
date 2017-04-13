37663108fbc684d37a7e2f65b9210ed8fa9ee251
Wyatt Robinson <thewyatt@gmail.com>
2017-04-13 02:40:58 +0000

    Added admin categoris folder. Added categories_controller in admin controllers folder.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
+
+  def
+
+end
