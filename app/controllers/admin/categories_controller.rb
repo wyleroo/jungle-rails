130c1c1f648f04a257417798ed27a0b2289e6683
Wyatt Robinson <thewyatt@gmail.com>
2017-04-13 18:19:05 +0000

    Added dropdown link to admin categories. Added routes for admin category delete, index, and new. Debugging routes.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,29 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  def index
+    @category = Category.all.order(created_at: :desc)
   end
 
-  def
+  def create
+    @category = Category.new(category_params)
+    if @product.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :products], notice: 'Category deleted!'
+  end
+
+  private
+  def category_params
+    params.require(:categories).permit(
+      :name,
+      )
+  end
 
 end
