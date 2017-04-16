701376f6dfa8eb119362fe3e4ce7f6d2d2b7f679
Wyatt Robinson <thewyatt@gmail.com>
2017-04-16 16:15:48 +0000

    Product reviews now viewable only for logged in users.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
 
-def create
-  raise "Yay, I'm here!"
-end
+  def create
+    review = Review.new(review_params)
+    review.user_id = session[:user_id]
+    review.product_id = params[:product_id]
+    if review.save
+      redirect_to product_path(params[:product_id])
+    else
+      redirect_to product_path(params[:product_id])
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :description)
+  end
 
 end
