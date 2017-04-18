ca4983607a7248d539646b2a1f0f5af22c6fd4ca
Wyatt Robinson <thewyatt@gmail.com>
2017-04-18 22:08:17 +0000

    Added testing for cart status in main page to show '1' after click Add. Test passing

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+
+    visit root_path
+
+    find('a.btn.btn-primary', match: :first).click
+    puts page.html
+
+    save_screenshot
+
+    # DEBUG / VERIFY
+    within('nav') { expect(page).to have_content('My Cart (1)') }
+  end
+end
